c908d6ab6e150cd6182a72b8d5f58ae04e43ee87
Rob Easthope <hello@robeasthope.com>
2023-07-05 20:26:01 +0100

    Trim page query

--- app/components/generic/Page/Page.query.ts
+++ app/components/generic/Page/Page.query.ts
@@ -2,9 +2,6 @@ import groq from 'groq';
 
 import type { SanityPageByIdQueryProps } from '~/types/SanityPageByIdQueryProps';
 
-import { SANITY_BLOCK_QUERIES } from '~/components/_base/SanityBlocks/SANITY_BLOCK_QUERIES';
-import { SANITY_IMAGE_QUERY } from '~/components/_base/SanityImage/SanityImage.query';
-
 // Fetch all page slugs
 export const PAGE_SLUGS_QUERY = groq`
   *[_type == "Page"  && defined(slug.current)].slug.current
@@ -30,8 +27,6 @@ export const PAGE_BY_ID_QUERY = ({
   id,
   componentTypes = [],
 }: SanityPageByIdQueryProps) => {
-  const hydratedSanityBlockQueries: any = SANITY_BLOCK_QUERIES();
-
   return groq`{
     "page": *[_id == "${id}"][0]{
       _id,
@@ -39,18 +34,6 @@ export const PAGE_BY_ID_QUERY = ({
       slug,
       metadataDescription,
       metadataImage,
-      "sections": rawSections[_type in [${componentTypes?.map(
-        (type: string) => `"${type}"`,
-      )}]]{
-        ${componentTypes
-          .map(
-            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-            (type: string) =>
-              // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
-              `_type == "${type}" => ${hydratedSanityBlockQueries[type]}`,
-          )
-          .join(',')}
-      }
     },
   }`;
 };
