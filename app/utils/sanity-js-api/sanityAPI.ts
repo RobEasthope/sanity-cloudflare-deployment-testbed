4de6b1239c9f55e43f5a02b105b883c20fe65e2d
Rob Easthope <hello@robeasthope.com>
2023-07-06 11:34:51 +0100

    Refactor typing

--- app/utils/sanity-js-api/sanityAPI.ts
+++ app/utils/sanity-js-api/sanityAPI.ts
@@ -1,13 +1,15 @@
 import { createClient } from '@sanity/client';
 
-export const sanityAPI = (sanityApiConfig: {
+export type SanityApiEnvVarsProps = {
   SANITY_PUBLIC_PROJECT_ID: string;
   SANITY_PUBLIC_DATASET: string;
   SANITY_PUBLIC_API_VERSION: string;
   SANITY_PERSPECTIVE: string;
   USE_SANITY_CDN: boolean;
   SANITY_API_TOKEN: string;
-}) =>
+};
+
+export const sanityAPI = (sanityApiConfig: SanityApiEnvVarsProps) =>
   createClient({
     projectId: sanityApiConfig.SANITY_PUBLIC_PROJECT_ID,
     dataset: sanityApiConfig.SANITY_PUBLIC_DATASET,
