38a21141f2ab2d2d3eae15f525d3363d7fd61020
Rob Easthope <hello@robeasthope.com>
2023-07-06 11:30:47 +0100

    Update Sanity API to accept props

--- app/utils/sanity-js-api/sanityAPI.ts
+++ app/utils/sanity-js-api/sanityAPI.ts
@@ -1,10 +1,18 @@
 import { createClient } from '@sanity/client';
 
-export const sanityAPI = createClient({
-  projectId: process.env.SANITY_PUBLIC_PROJECT_ID || '',
-  dataset: process.env.SANITY_PUBLIC_DATASET || '',
-  apiVersion: process.env.SANITY_PUBLIC_API_VERSION || '',
-  useCdn: true,
-  perspective: 'published',
-  token: process.env.SANITY_API_TOKEN || '',
-});
+export const sanityAPI = (sanityApiConfig: {
+  SANITY_PUBLIC_PROJECT_ID: string;
+  SANITY_PUBLIC_DATASET: string;
+  SANITY_PUBLIC_API_VERSION: string;
+  SANITY_PERSPECTIVE: string;
+  USE_SANITY_CDN: boolean;
+  SANITY_API_TOKEN: string;
+}) =>
+  createClient({
+    projectId: sanityApiConfig.SANITY_PUBLIC_PROJECT_ID,
+    dataset: sanityApiConfig.SANITY_PUBLIC_DATASET,
+    apiVersion: sanityApiConfig.SANITY_PUBLIC_API_VERSION,
+    useCdn: true,
+    perspective: sanityApiConfig.SANITY_PERSPECTIVE,
+    token: sanityApiConfig.SANITY_API_TOKEN,
+  });
