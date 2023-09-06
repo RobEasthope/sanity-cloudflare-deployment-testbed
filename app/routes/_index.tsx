7392c108953ea54653408b9aac823cf84d5d8818
Rob Easthope <hello@robeasthope.com>
2023-09-06 15:09:11 +0100

    Add basic loader code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,39 @@
-import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import type { V2_MetaFunction } from '@remix-run/cloudflare';
+import { json } from '@remix-run/cloudflare';
+import groq from 'groq';
+import { sanityAPI } from '~/utils/sanity-js-api/sanityAPI';
+
+export async function loader() {
+  const payload = await sanityAPI({ preview: false }).fetch(
+    groq`*[_type in ["Page"] && slug.current == 'home'][0]{
+      title
+    }`,
+  );
+
+  if (!payload) {
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw new Response('Not Found', {
+      status: 404,
+    });
+  }
+
+  return json({
+    page: payload || null,
+  });
+}
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export default function Index() {
+  const { page } = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1>{page?.title}</h1>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
